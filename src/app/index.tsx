116f3c8a0fb754efd0347cf68b62713dc2f83065
myti1234 <myticaturla@gmail.com>
2026-03-20 14:32:19 +0800

    feat: initialize grocery app with Expo, add basic layout and index screen
    - Created package.json with dependencies and scripts for Expo project
    - Added RootLayout component for navigation stack
    - Implemented Index screen with a link to about screen and an image
    - Configured TypeScript with tsconfig.json for strict type checking and path aliases

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,14 @@
 import { Image } from "expo-image";
-import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import "../../global.css";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen. 123</Text>
-      <Link href={"/about"}>take about screen</Link>
+      <Text className="text-red-300 text-4xl bg-purple-500">
+        Edit src/app/index.tsx to edit this screen. 123
+      </Text>
+
       <Image
         source={require("../../assets/images/terry.png")}
         style={{ width: 200, height: 200, borderRadius: 20 }}
